3b1d2158c9f682a3ab58283f36f60469ed6af7f2
Rafa <nero@mpn83.maypun-it-com.ar>
2011-08-03 23:46:59 -0300

    +migrations...

---
+++ db/migrate/20110802144009_create_incomes.rb
@@ -0,0 +1,15 @@
+class CreateIncomes < ActiveRecord::Migration
+  def self.up
+    create_table :incomes do |t|
+
+      t.integer :amount
+      t.references :account
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :incomes
+  end
+end
